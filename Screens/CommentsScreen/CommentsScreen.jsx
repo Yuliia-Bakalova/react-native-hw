2a76baa933eeb27062b229c9636a93a5d263915b
Yuliia-Bakalova <98843051+Yuliia-Bakalova@users.noreply.github.com>
2023-03-06 16:20:00 +0200

    fix

--- Screens/CommentsScreen/CommentsScreen.jsx
+++ Screens/CommentsScreen/CommentsScreen.jsx
@@ -5,14 +5,87 @@ import { TextInput } from "react-native-gesture-handler";
 import { Feather } from "@expo/vector-icons";
 import db from "../../firebase/config";
 import { styles } from "./CommentsScreen.styled";
+import { getLogin } from "../../redux/auth/selectors";
 
+// const CommentsScreen = ({ route }) => {
+//   const [comment, setComment] = useState("");
+//   const [allComments, setAllComments] = useState(null);
+//   const photo = route.params.item.photo;
+//   const postId = route.params.item.id;
+//   const login = useSelector((state) => state.auth.login);
+//   const item = route.params.item;
+
+//   useEffect(() => {
+//     getAllPosts();
+//   }, []);
+
+//   const createPost = async () => {
+//     console.log(item);
+//     const date = new Date().toLocaleString();
+//     db.firestore()
+//       .collection("posts")
+//       .doc(postId)
+//       .collection("comments")
+//       .add({ comment, login, date });
+//     setComment("");
+//     db.firestore()
+//       .collection("posts")
+//       .doc(postId)
+//       .set({ ...item, commentsCount: allComments.length + 1 });
+//   };
+
+//   const getAllPosts = async () => {
+//     db.firestore()
+//       .collection("posts")
+//       .doc(postId)
+//       .collection("comments")
+//       .onSnapshot((data) =>
+//         setAllComments(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+//       );
+//   };
+//   return (
+//     <View style={styles.container}>
+//        <Image source={{ uri: photo }} style={styles.image} />
+//       <SafeAreaView style={styles.container2}>
+//         <FlatList
+//           data={allComments}
+//           renderItem={({ item }) => (
+//             <View style={{ flex: 1, flexDirection: "row" }}>
+//               <View style={styles.commentContainer}>
+//                 <Text>{item.login}</Text>
+//                 <Text>{item.comment}</Text>
+//                 <Text>{item.date}</Text>
+//               </View>
+//               <View style={styles.avatar}></View>
+//             </View>
+//           )}
+//           keyExtractor={(item) => item.id}
+//         />
+//       </SafeAreaView>
+//       <View>
+//         <TextInput
+//           value={comment}
+//           onChangeText={setComment}
+//           placeholder="Comment.."
+//           placeholderTextColor="#BDBDBD"
+//           style={styles.input}
+//         />
+//         <TouchableOpacity style={styles.sendBtn} onPress={createPost}>
+//           <Feather name="arrow-up" size={22} color="#fff" />
+//         </TouchableOpacity>
+//       </View>
+//       </View>
+//   );
+// };
+
+// export default CommentsScreen;
 
 const CommentsScreen = ({ route }) => {
   const [comment, setComment] = useState("");
   const [allComments, setAllComments] = useState(null);
   const photo = route.params.item.photo;
   const postId = route.params.item.id;
-  const login = useSelector((state) => state.auth.login);
+  const login = useSelector(getLogin);
   const item = route.params.item;
 
   useEffect(() => {
@@ -45,16 +118,16 @@ const CommentsScreen = ({ route }) => {
   };
   return (
     <View style={styles.container}>
-       <Image source={{ uri: photo }} style={styles.image} />
+      <Image source={{ uri: photo }} style={styles.image} />
       <SafeAreaView style={styles.container2}>
         <FlatList
           data={allComments}
           renderItem={({ item }) => (
             <View style={{ flex: 1, flexDirection: "row" }}>
               <View style={styles.commentContainer}>
-                <Text>{item.login}</Text>
-                <Text>{item.comment}</Text>
-                <Text>{item.date}</Text>
+                <Text style={styles.commentLogin}>{item.login}</Text>
+                <Text style={styles.commentText}>{item.comment}</Text>
+                <Text style={styles.commentDate}>{item.date}</Text>
               </View>
               <View style={styles.avatar}></View>
             </View>
@@ -74,10 +147,8 @@ const CommentsScreen = ({ route }) => {
           <Feather name="arrow-up" size={22} color="#fff" />
         </TouchableOpacity>
       </View>
-      </View>
+    </View>
   );
 };
 
 export default CommentsScreen;
-
-
--- Screens/CreatePostsScreen/CreatePostsScreen.jsx
+++ Screens/CreatePostsScreen/CreatePostsScreen.jsx
@@ -6,26 +6,159 @@ import {
   Keyboard,
   Image,
 } from "react-native";
-import { styles } from "./CreatePostsScreen.styled";
-import { MaterialIcons } from "@expo/vector-icons";
 import { useEffect, useState  } from "react";
 import { Camera } from "expo-camera";
 import * as Location from "expo-location";
-import { useSelector } from "react-redux";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import { TextInput } from "react-native-gesture-handler";
+import { useSelector } from "react-redux";
+import * as ImagePicker from "expo-image-picker";
+import { getLogin, getUserId } from "../../redux/auth/selectors";
 import db from "../../firebase/config";
 
 
+import { styles } from "./CreatePostsScreen.styled";
+
+
+
+// const CreatePostsScreen = ({ navigation }) => {
+//   const [camera, setCamera] = useState(null);
+//   const [photo, setPhoto] = useState(null);
+
+//   const [location, setLocation] = useState(null);
+//   const [publishBtnColor, setPublishBtnColor] = useState("#f6f6f6");
+//   const [publishBtnColorText, setPublishBtnColorText] = useState("#BDBDBD");
+//    const [description, setDescription] = useState("");
+//   const [place, setPlace] = useState("");
+
+//   useEffect(() => {
+//     (async () => {
+//       let { status } = await Camera.requestCameraPermissionsAsync();
+//       if (status !== "granted") {
+//         console.log("Permission to access location was denied");
+//         return;
+//       }
+//        let locationRes = await Location.getCurrentPositionAsync({});
+//       setLocation(locationRes);
+//     })();
+//   }, []);
+
+//   useEffect(() => {
+//     (async () => {
+//       let { status } = await Location.requestForegroundPermissionsAsync();
+//       if (status !== "granted") {
+//         console.log("Permission to access location was denied");
+//         return;
+//       }
+//     })();
+//   }, []);
+
+//   const keyboardHide = () => {
+//     Keyboard.dismiss();
+//   };
+
+// const takePhoto = async () => {
+//     const snap = await camera.takePictureAsync();
+//     setPublishBtnColor("#FF6C00");
+//     setPublishBtnColorText("#FFF");
+//     const loc = await Location.getCurrentPositionAsync();
+//     setPhoto(snap.uri);
+//     console.log(loc);
+//     setLocation(loc.coords);
+//   };
+
+//   const sendPhoto = () => {
+//     uploadPostToServer();
+//     navigation.navigate("Posts");
+//     setDescription("");
+//     setPlace("");
+//     setPhoto(null);
+//   };
+
+//   const login = useSelector((state) => state.auth.login);
+//   const userId = useSelector((state) => state.auth.userIderId);
+
+//   const uploadPostToServer = async () => {
+//     const photo = await uploadPhotoToServer();
+//     console.log(location);
+//     const createPost = await db
+//       .firestore()
+//       .collection("posts")
+//       .add({ photo, place, description, location, userId, login });
+//   };
+
+//   const uploadPhotoToServer = async () => {
+//     const response = await fetch(photo);
+//     const file = await response.blob();
+//     const uniquePostId = Date.now().toString();
+//     await db.storage().ref(`postImage/${uniquePostId}`).put(file);
+//     const processedPhoto = await db
+//       .storage()
+//       .ref("postImage")
+//       .child(uniquePostId)
+//       .getDownloadURL();
+
+//     console.log("processedPhoto", processedPhoto);
+//     return processedPhoto;
+//   };
+//   return (
+//     <TouchableWithoutFeedback onPress={keyboardHide}>
+//       <View style={styles.container}>
+//         <Camera style={styles.photoWrap} ref={setCamera}>
+//           {photo && (
+//             <View style={styles.takePhotoWrap}>
+//               <Image
+//                 source={{ uri: photo }}
+//                 style={{ height: 240, width: 360 }}
+//               />
+//             </View>
+//           )}
+
+//           <TouchableOpacity style={styles.iconWrap} onPress={takePhoto}>
+//             <MaterialIcons name="camera-alt" size={30} color="#BDBDBD" />
+//          </TouchableOpacity>
+//         </Camera>
+//         <Text style={styles.text}>Загрузите фото</Text>
+//         <TextInput
+//           placeholder="Название..."
+//           placeholderTextColor="#BDBDBD"
+//           style={styles.input}
+//           value={description}
+//           onChangeText={setDescription}
+//         />
+//         <TextInput
+//           placeholder="Местность..."
+//           placeholderTextColor="#BDBDBD"
+//           style={{ ...styles.input, marginBottom: 32 }}
+//             value={place}
+//           onChangeText={setPlace}
+//         />
+
+//         <TouchableOpacity
+//           onPress={sendPhoto}
+//           activeOpacity={0.7}
+//           style={{ ...styles.publishBtn, backgroundColor: publishBtnColor }}
+//         >
+//           <Text
+//             style={{ ...styles.publishBtnText, color: publishBtnColorText }}
+//           >
+//             Опубликовать
+//           </Text>
+//         </TouchableOpacity>
+//       </View>
+//     </TouchableWithoutFeedback>
+//   );
+// };
+
+// export default CreatePostsScreen;
 
 
 const CreatePostsScreen = ({ navigation }) => {
   const [camera, setCamera] = useState(null);
   const [photo, setPhoto] = useState(null);
-
   const [location, setLocation] = useState(null);
-  const [publishBtnColor, setPublishBtnColor] = useState("#f6f6f6");
-  const [publishBtnColorText, setPublishBtnColorText] = useState("#BDBDBD");
-   const [description, setDescription] = useState("");
+  const [description, setDescription] = useState("");
   const [place, setPlace] = useState("");
 
   useEffect(() => {
@@ -35,8 +168,6 @@ const CreatePostsScreen = ({ navigation }) => {
         console.log("Permission to access location was denied");
         return;
       }
-       let locationRes = await Location.getCurrentPositionAsync({});
-      setLocation(locationRes);
     })();
   }, []);
 
@@ -47,6 +178,8 @@ const CreatePostsScreen = ({ navigation }) => {
         console.log("Permission to access location was denied");
         return;
       }
+      let locationRes = await Location.getCurrentPositionAsync({});
+      setLocation(locationRes);
     })();
   }, []);
 
@@ -54,13 +187,11 @@ const CreatePostsScreen = ({ navigation }) => {
     Keyboard.dismiss();
   };
 
-const takePhoto = async () => {
+  const takePhoto = async () => {
     const snap = await camera.takePictureAsync();
-    setPublishBtnColor("#FF6C00");
-    setPublishBtnColorText("#FFF");
     const loc = await Location.getCurrentPositionAsync();
     setPhoto(snap.uri);
-    console.log(loc);
+
     setLocation(loc.coords);
   };
 
@@ -72,12 +203,11 @@ const takePhoto = async () => {
     setPhoto(null);
   };
 
-  const login = useSelector((state) => state.auth.login);
-  const userId = useSelector((state) => state.auth.userIderId);
+  const login = useSelector(getLogin);
+  const userId = useSelector(getUserId);
 
   const uploadPostToServer = async () => {
     const photo = await uploadPhotoToServer();
-    console.log(location);
     const createPost = await db
       .firestore()
       .collection("posts")
@@ -95,52 +225,96 @@ const takePhoto = async () => {
       .child(uniquePostId)
       .getDownloadURL();
 
-    console.log("processedPhoto", processedPhoto);
     return processedPhoto;
   };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setPhoto(result.assets[0].uri);
+    }
+  };
+
+  const clearPost = () => {
+    setDescription("");
+    setPhoto(null);
+    setPlace("");
+  };
+
   return (
     <TouchableWithoutFeedback onPress={keyboardHide}>
       <View style={styles.container}>
-        <Camera style={styles.photoWrap} ref={setCamera}>
-          {photo && (
-            <View style={styles.takePhotoWrap}>
-              <Image
-                source={{ uri: photo }}
-                style={{ height: 240, width: 360 }}
-              />
-            </View>
-          )}
+        <View>
+          <Camera style={styles.photoWrap} ref={setCamera}>
+            {photo && (
+              <View style={styles.takePhotoWrap}>
+                <Image
+                  source={{ uri: photo }}
+                  style={{ height: 240, width: 360 }}
+                />
+              </View>
+            )}
 
-          <TouchableOpacity style={styles.iconWrap} onPress={takePhoto}>
-            <MaterialIcons name="camera-alt" size={30} color="#BDBDBD" />
-         </TouchableOpacity>
-        </Camera>
-        <Text style={styles.text}>Загрузите фото</Text>
-        <TextInput
-          placeholder="Название..."
-          placeholderTextColor="#BDBDBD"
-          style={styles.input}
-          value={description}
-          onChangeText={setDescription}
-        />
-        <TextInput
-          placeholder="Местность..."
-          placeholderTextColor="#BDBDBD"
-          style={{ ...styles.input, marginBottom: 32 }}
+            <TouchableOpacity style={styles.iconWrap} onPress={takePhoto}>
+              <MaterialIcons name="camera-alt" size={30} color="#BDBDBD" />
+            </TouchableOpacity>
+          </Camera>
+          <TouchableOpacity onPress={pickImage}>
+            {!photo ? (
+              <Text style={styles.text}>Upload photo</Text>
+            ) : (
+              <Text style={styles.text}>Change photo</Text>
+            )}
+          </TouchableOpacity>
+
+          <TextInput
+            placeholder="Name..."
+            placeholderTextColor="#BDBDBD"
+            style={styles.input}
+            value={description}
+            onChangeText={setDescription}
+          />
+          <TextInput
+            placeholder="Place..."
+            placeholderTextColor="#BDBDBD"
+            style={{ ...styles.input, marginBottom: 32 }}
             value={place}
-          onChangeText={setPlace}
-        />
+            onChangeText={setPlace}
+          />
 
-        <TouchableOpacity
-          onPress={sendPhoto}
-          activeOpacity={0.7}
-          style={{ ...styles.publishBtn, backgroundColor: publishBtnColor }}
-        >
-          <Text
-            style={{ ...styles.publishBtnText, color: publishBtnColorText }}
+          <TouchableOpacity
+            disabled={photo ? false : true}
+            onPress={sendPhoto}
+            activeOpacity={0.7}
+            style={
+              photo
+                ? { ...styles.publishBtn, backgroundColor: "#124250" }
+                : styles.publishBtn
+            }
           >
-            Опубликовать
-          </Text>
+            <Text
+              style={
+                photo
+                  ? { ...styles.publishBtnText, color: "#fff" }
+                  : styles.publishBtnText
+              }
+            >
+              Publish
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          onPress={clearPost}
+          activeOpacity={0.7}
+          style={styles.deleteBtn}
+        >
+          <Feather name="trash-2" size={24} color="#BDBDBD" />
         </TouchableOpacity>
       </View>
     </TouchableWithoutFeedback>
